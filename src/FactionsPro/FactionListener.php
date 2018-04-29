7151d2b3a8e4f172348ca8045cb9a3718ed9e417
TheFixerDevelopment <30442910+TheFixerDevelopment@users.noreply.github.com>
2018-04-29 21:42:34 +0100

    Fixed Syntax error.

--- src/FactionsPro/FactionListener.php
+++ src/FactionsPro/FactionListener.php
@@ -230,7 +230,7 @@ class FactionListener implements Listener {
     $y = floor($event->getPlayer()->getY());
     $z = floor($event->getPlayer()->getZ());
 	       
-    if($this->plugin->drawPlot($event->getPlayer(), $faction, $x, $y, $z, $event->getPlayer()->getLevel()->getName(), $this->plugin->prefs->get("PlotSize")) {
+    if($this->plugin->drawPlot($event->getPlayer(), $faction, $x, $y, $z, $event->getPlayer()->getLevel()->getName(), $this->plugin->prefs->get("PlotSize"))) {
     $event->getPlayer()->addTitle("You have just left", "A Faction's plot!", 40, 20, 40);
     }
     }
