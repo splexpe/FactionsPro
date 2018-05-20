cc71a8b551cb2f088e2c93258f47f4cb296ac453
iZeaoGamer <jameshayward14@icloud.com>
2018-05-20 13:43:19 +0100

    Fix MOTDTime.

--- src/FactionsPro/FactionListener.php
+++ src/FactionsPro/FactionListener.php
@@ -26,7 +26,7 @@ class FactionListener implements Listener {
 		if($this->plugin->motdWaiting($playerName)) {
 			$motdtime = $this->plugin->prefs->get("MOTDTime");
 			$prefix = $this->plugin->prefs->get("pluginprefix");
-			if(time() - $this->plugin->getMOTDTime($playerName) > 60) {
+			if(time() - $this->plugin->getMOTDTime($playerName) > $motdtime) {
 				$PCE->getPlayer()->sendMessage($this->plugin->formatMessage("$prefix §cTimed out. §bPlease use: §3/f desc again."));
 				$this->plugin->db->query("DELETE FROM motdrcv WHERE player='$playerName';");
 				$PCE->setCancelled(true);
