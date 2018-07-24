edf9fdf2204082979e9cd9cace88eef4676614bb
TheFixerDevelopment <30442910+TheFixerDevelopment@users.noreply.github.com>
2018-07-24 15:24:29 +0100

    Commands: Revamped const code (Untested)

--- src/FactionsPro/FactionCommands.php
+++ src/FactionsPro/FactionCommands.php
@@ -13,14 +13,15 @@ class FactionCommands {
     public $plugin;
     
     // ASCII Map
-	CONST MAP_WIDTH = 50;
-	CONST MAP_HEIGHT = 11;
-	CONST MAP_HEIGHT_FULL = 17;
-	CONST MAP_KEY_CHARS = "\\/#?ç¬£$%=&^ABCDEFGHJKLMNOPQRSTUVWXYZÄÖÜÆØÅ1234567890abcdeghjmnopqrsuvwxyÿzäöüæøåâêîûô";
-	CONST MAP_KEY_WILDERNESS = TextFormat::GRAY . "-"; /*Del*/
-	CONST MAP_KEY_SEPARATOR = TextFormat::AQUA . "*"; /*Del*/
-	CONST MAP_KEY_OVERFLOW = TextFormat::WHITE . "-" . TextFormat::WHITE; # ::MAGIC?
-	CONST MAP_OVERFLOW_MESSAGE = self::MAP_KEY_OVERFLOW . ": Too Many Factions (>" . 107 . ") on this Map.";
+	public const
+	MAP_WIDTH = 50,
+	MAP_HEIGHT = 11,
+	MAP_HEIGHT_FULL = 17,
+	MAP_KEY_CHARS = "\\/#?ç¬£$%=&^ABCDEFGHJKLMNOPQRSTUVWXYZÄÖÜÆØÅ1234567890abcdeghjmnopqrsuvwxyÿzäöüæøåâêîûô",
+	MAP_KEY_WILDERNESS = TextFormat::GRAY . "-", /*Del*/
+	MAP_KEY_SEPARATOR = TextFormat::AQUA . "*", /*Del*/
+	MAP_KEY_OVERFLOW = TextFormat::WHITE . "-" . TextFormat::WHITE, # ::MAGIC?
+	MAP_OVERFLOW_MESSAGE = self::MAP_KEY_OVERFLOW . ": Too Many Factions (>" . 107 . ") on this Map.";
         
     public function __construct(FactionMain $pg) {
         $this->plugin = $pg;
@@ -1532,13 +1533,15 @@ class FactionCommands {
 		return "§c";
 	}
 	   const N = 'N';
-    const NE = '/';
-    const E = 'E';
-    const SE = '\\';
-    const S = 'S';
-    const SW = '/';
-    const W = 'W';
-    const NW = '\\';
+    public const
+	  NE = "/",
+    E = "E",
+    SE = "\\",
+    S = "S",
+    SW = "/",
+    W = "W",
+    NW = '\\';
+	
     public static function getASCIICompass($degrees, $colorActive, $colorDefault) : array
     {
         $ret = [];
