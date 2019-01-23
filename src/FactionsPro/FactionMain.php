7ec13c0649cc21b28dc7443c557ace2c70eb1f91
iZeaoGamer <jameshayward14@icloud.com>
2019-01-23 23:03:08 +0000

    Update FactionMain.php

--- src/FactionsPro/FactionMain.php
+++ src/FactionsPro/FactionMain.php
@@ -491,7 +491,7 @@ foreach ($this->getServer()->getOnlinePlayers() as $players){
         $f = $this->getPlayerFaction($players->getName());
         if($this->isInFaction($players->getName())) {
 foreach ($this->getServer()->getOnlinePlayers() as $player){
-        $player->setScoreTag("§a$players->getName() §5| §a$f"); //To-Do make this configurable.
+        $player->setScoreTag("§a$player->getName() §5| §a$f"); //To-Do make this configurable.
         }
     }
 }
