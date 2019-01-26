7506d654f3398b5bf4973fbd59214870a5d71537
iZeaoGamer <jameshayward14@icloud.com>
2019-01-26 01:21:59 +0000

    Bug fix.

--- src/FactionsPro/FactionMain.php
+++ src/FactionsPro/FactionMain.php
@@ -131,7 +131,7 @@ class FactionMain extends PluginBase implements Listener {
 		}
     }
     public function tagCheck() : void{
-if($this->prefs->get("tag-type") == true or $this->prefs->get("tag-type") == false){ //to-do fix some bugs with this.
+if(!$this->prefs->get("tag-type")){ //to-do fix some bugs with this.
 $this->getLogger()->info("Tag-type has an invalid option. Either select ‘nametag’ or ‘scoretag’ in prefs.yml. Plugin disabled.");
 $this->getServer()->getPluginManager()->disablePlugin($this);
 return;
