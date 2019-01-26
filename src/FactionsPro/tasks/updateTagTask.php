31b5586bac23539e17fb7c1c9979a97f0c51bf66
iZeaoGamer <jameshayward14@icloud.com>
2019-01-26 13:41:11 +0000

    Added void return
    Removed dead variables.

--- src/FactionsPro/tasks/updateTagTask.php
+++ src/FactionsPro/tasks/updateTagTask.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace FactionsPro\tasks;
+
 use FactionsPro\FactionMain;
 use pocketmine\Server;
 use pocketmine\scheduler\Task;
@@ -13,13 +14,12 @@ class updateTagTask extends Task{
 	public function __construct(FactionMain $plugin){
 		$this->plugin = $plugin;
 	}
-	public function onRun(int $currentTick){
+	public function onRun(int $currentTick) : void{
 if($this->plugin->prefs->get("tag-type") == "scoretag"){
 		foreach(Server::getInstance()->getOnlinePlayers() as $player){
 			if($player instanceof Player){
 			$player->setNameTagVisible();
 			$f = $this->plugin->getPlayerFaction($player->getName());
-			$name = $player->getName();
 			$player->setScoreTag(str_replace(["{player}", "{faction}"], [$player->getName(), $f], $this->plugin->prefs->get("faction-tag")));
 } else {
 if($this->plugin->prefs->get("tag-type") == "nametag"){
@@ -27,8 +27,7 @@ foreach(Server::getInstance()->getOnlinePlayers() as $player){
 			if($player instanceof Player){
 			$player->setNameTagVisible();
 			$f = $this->plugin->getPlayerFaction($player->getName());
-			$name = $player->getName();
-			$player->setNameTag(str_replace(["{player}", "{faction}"], [$player->getName(), $f], $this->plugin->prefs->get("faction-tag")));
+			$player->setNameTag(str_replace(["{player}", "{faction}"], [$player->getName(), $f], $this->plugin->prefs->get("faction-tag"))); //to-do see if this will work.
 	                }
 		 }
 	      }
