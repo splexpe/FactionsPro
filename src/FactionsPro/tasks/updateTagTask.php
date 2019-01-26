5115a0c3b18ad51d5210860ae5b68f2c9bface7a
iZeaoGamer <jameshayward14@icloud.com>
2019-01-26 00:13:38 +0000

    Add tag-type options: scoreTag, and NameTag [1/2]

--- src/FactionsPro/tasks/updateTagTask.php
+++ src/FactionsPro/tasks/updateTagTask.php
@@ -14,13 +14,25 @@ class updateTagTask extends Task{
 		$this->plugin = $plugin;
 	}
 	public function onRun(int $currentTick){
+if($this->plugin->prefs->get("tag-type") == "scoretag"){
 		foreach(Server::getInstance()->getOnlinePlayers() as $player){
 			if($player instanceof Player){
 			$player->setNameTagVisible();
 			$f = $this->plugin->getPlayerFaction($player->getName());
 				$name = $player->getName();
 			$player->setScoreTag(str_replace(["{player}", "{faction}"], [$player->getName(), $f], $this->plugin->prefs->get("faction-tag")));
+} else {
+if($this->plugin->prefs->get("tag-type") == "nametag"){
+foreach(Server::getInstance()->getOnlinePlayers() as $player){
+			if($player instanceof Player){
+			$player->setNameTagVisible();
+			$f = $this->plugin->getPlayerFaction($player->getName());
+				$name = $player->getName();			$player->setNameTag(str_replace(["{player}", "{faction}"], [$player->getName(), $f], $this->plugin->prefs->get("faction-tag")));
 			}
 		}
 	}
+}
+}
+}
+}
 }
