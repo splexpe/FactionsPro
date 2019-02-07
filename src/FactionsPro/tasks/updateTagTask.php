a8b05f5387ed43967e959623397fd6f6d63e62d9
iZeaoGamer <jameshayward14@icloud.com>
2019-02-07 19:35:21 +0000

    FactionTags update -- HOVER TO SEE
    * Fixed faction tags showing even if the faction is deleted. This no longer prints empty faction names onto your tag, which prevents blank faction names.
    * Added new option: enable-not-created-hud - Allows you to enable/disable a hud (if faction hasn't been created)
    * Added new option: hud-message - Allows you to edit the hud message if the faction isn't created. (If you have enable-not-created-hud option enabled.)
    * Added new file: FactionDeleteTask - I will try and make this better by renaming it, but it should also work with anything that relates to a player not in a faction.
    * Bug fixes and many improvements.

--- src/FactionsPro/tasks/updateTagTask.php
+++ src/FactionsPro/tasks/updateTagTask.php
@@ -1,67 +1,1 @@
-<?php
-
-namespace FactionsPro\tasks;
-
-use FactionsPro\FactionMain;
-use pocketmine\Server;
-use pocketmine\scheduler\Task;
-use pocketmine\Player;
-use pocketmine\utils\TextFormat;
-
-class updateTagTask extends Task{
-	
-	public $plugin;
-	
-	public function __construct(FactionMain $plugin){
-		$this->plugin = $plugin;
-	}
-	public function onRun(int $currentTick) : void{
-if($this->plugin->prefs->get("tag-type") == "scoretag"){
-		foreach(Server::getInstance()->getOnlinePlayers() as $player){
-			if($player instanceof Player){
-			$player->setNameTagVisible();
-			$f = $this->plugin->getPlayerFaction($player->getName());
-				if($this->plugin->isInFaction($player->getName()) == true) {
-				$player->setScoreTag(str_replace(["{player}", "{faction}"], [$player->getName(), $f], $this->plugin->prefs->get("faction-tag")));
-					} else {
-				if($this->plugin->isInFaction($player->getName()) == false){
-					$player->addActionBarMessage(TextFormat::colorize("&cYou have currently not made a faction. &aUse: &b/f create <faction>")); //To-Do add options for this (enable / disable), and be able to edit the messages.
-} else {
-if($this->plugin->prefs->get("tag-type") == "nametag"){
-foreach(Server::getInstance()->getOnlinePlayers() as $player){
-			if($player instanceof Player){
-			$player->setNameTagVisible();
-			$f = $this->plugin->getPlayerFaction($player->getName());
-				if($this->plugin->isInFaction($player->getName()) == true) {
-				$player->setNameTag(str_replace(["{player}", "{faction}"], [$player->getName(), $f], $this->plugin->prefs->get("faction-tag"))); 
-					} else {
-				if($this->plugin->isInFaction($player->getName()) == false){
-					$player->addActionBarMessage(TextFormat::colorize("&cYou have currently not made a faction. &aUse: &b/f create <faction>")); //To-Do add options for this (enable / disable), and be able to edit the messages.
-				} else {
-if($this->plugin->prefs->get("tag-type") == "displaytag"){
-foreach(Server::getInstance()->getOnlinePlayers() as $player){
-			if($player instanceof Player){
-			$player->setNameTagVisible();
-			$f = $this->plugin->getPlayerFaction($player->getName());
-				if($this->plugin->isInFaction($player->getName()) == true) {
-				$player->setDisplayName(str_replace(["{player}", "{faction}"], [$player->getName(), $f], $this->plugin->prefs->get("faction-tag")));
-			} else {
-				if($this->plugin->isInFaction($player->getName()) == false){
-					$player->addActionBarMessage(TextFormat::colorize("&cYou have currently not made a faction. &aUse: &b/f create <faction>")); //To-Do add options for this (enable / disable), and be able to edit the messages.
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
+<?phpnamespace FactionsPro\tasks;use FactionsPro\FactionMain;use pocketmine\Server;use pocketmine\scheduler\Task;use pocketmine\Player;use pocketmine\utils\TextFormat;class updateTagTask extends Task{		public $plugin;		public function __construct(FactionMain $plugin){		$this->plugin = $plugin;	}	public function onRun(int $currentTick) : void{if($this->plugin->prefs->get("tag-type") == "scoretag"){		foreach(Server::getInstance()->getOnlinePlayers() as $player){			if($player instanceof Player){			$player->setNameTagVisible();			$f = $this->plugin->getPlayerFaction($player->getName());				if($this->plugin->isInFaction($player->getName()) == true) {				$player->setScoreTag(str_replace(["{player}", "{faction}"], [$player->getName(), $f], $this->plugin->prefs->get("faction-tag")));					} else {				if($this->plugin->isInFaction($player->getName()) == false){				    if($this->plugin->prefs->get("enable-not-created-hud") == true){					$player->addActionBarMessage(TextFormat::colorize($this->plugin->prefs->get("hud-message")));} else {if($this->plugin->prefs->get("tag-type") == "nametag"){foreach(Server::getInstance()->getOnlinePlayers() as $player){			if($player instanceof Player){			$player->setNameTagVisible();			$f = $this->plugin->getPlayerFaction($player->getName());				if($this->plugin->isInFaction($player->getName()) == true) {				$player->setNameTag(str_replace(["{player}", "{faction}"], [$player->getName(), $f], $this->plugin->prefs->get("faction-tag"))); 					} else {				if($this->plugin->isInFaction($player->getName()) == false){				 if($this->plugin->prefs->get("enable-not-created-hud") == true){					$player->addActionBarMessage(TextFormat::colorize($this->plugin->prefs->get("hud-message")));				} else {if($this->plugin->prefs->get("tag-type") == "displaytag"){foreach(Server::getInstance()->getOnlinePlayers() as $player){			if($player instanceof Player){			$player->setNameTagVisible();			$f = $this->plugin->getPlayerFaction($player->getName());				if($this->plugin->isInFaction($player->getName()) == true) {				$player->setDisplayName(str_replace(["{player}", "{faction}"], [$player->getName(), $f], $this->plugin->prefs->get("faction-tag")));			} else {				if($this->plugin->isInFaction($player->getName()) == false){					 if($this->plugin->prefs->get("enable-not-created-hud") == true){					$player->addActionBarMessage(TextFormat::colorize($this->plugin->prefs->get("hud-message")));}}}}}}}}}}}}}}}}}}}}
